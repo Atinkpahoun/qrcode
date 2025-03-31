2106cf375ea84a855850d5120f1d0cb9de7cbee1
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2025-03-31 16:43:32 +0100

    Select

--- src/pages/Historique.jsx
+++ src/pages/Historique.jsx
@@ -12,17 +12,19 @@ function Historique(){
         <div className=" pt-5 xl:pt-14 justify-center">
             <h1 className="doto text-2xl xl:text-3xl semibold text-[#0000FF] text-center">Bienvenu(e) dans l'historique de vos codes QR</h1>
             <div className="flex items-center justify-center mt-7 xl:mt-16">
-                <select 
-                    value={selectedOption} 
-                    onChange={handleChange} 
-                    className="bg-blue-50 border-2 border-[#0000FF] rounded-md p-2 focus:outline-none focus:ring-1 focus:ring-[#0000FF]"
-                >
-                    <option value="URL">URL</option>
-                    <option value="email">Email</option>
-                    <option value="texte">Texte</option>
-                    <option value="téléphone">Téléphone</option>
-                    <option value="image">Image</option>
-                </select>
+            <select 
+                value={selectedOption} 
+                onChange={handleChange} 
+                className="bg-blue-50 border-2 border-[#0000FF] rounded-md p-2 focus:outline-none focus:ring-1 focus:ring-[#0000FF]"
+            >
+                <option value="tous">Tous les codes QR</option>
+                <option value="URL">URL</option>
+                <option value="email">Email</option>
+                <option value="texte">Texte</option>
+                <option value="téléphone">Téléphone</option>
+                <option value="image">Image</option>
+            </select>
+
             </div>
             <div className="mt-5 lg:mt-10 mb-2">
                 <div className="mx-2 lg:mx-5 xl:mx-10 text-sm xl:text-xl doto text-[#0000FF] bg-blue-50 rounded border-2 border-[#0000FF] py-2 px-2 justify-between flex">
