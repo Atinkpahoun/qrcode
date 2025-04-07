862f3fe13a8740620e600d2623f483329fd844d0
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2025-04-07 12:53:23 +0100

    ButtonCommencer

---
+++ src/composants/ButtonCommencer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ButtonCommencer() {
+  return (
+    <div>
+        <button className="bg-[#0000FF] text-white px-4 lg:px-6 py-2  text-start font-bold rounded-lg doto text-lg lg:text-2xl"><a href="/src/pages/CodeQR.jsx">Commencer</a></button>
+    </div>
+  )
+}
+
+export default ButtonCommencer
