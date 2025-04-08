99e3f56557025191f230eaa8c955e96af8e0a173
Obed <obedamoussou2003@gmail.com>
2025-04-08 17:04:02 +0100

    wOw

---
+++ .history/src/composants/Upload_20250408124712.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { FaChevronDown } from 'react-icons/fa';
+const UploadMenu = ({ onLogoChange }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImage(reader.result);
+      onLogoChange(reader.result, setTempLogoTaille); // 🔹 Envoie la mise à jour au parent
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleSizeChange = (taille) => {
+    setTempLogoTaille(taille);
+    onLogoChange(image, taille); // 🔹 Met à jour le parent
+  };
+
+  const toggleMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
+ 
+  return (
+    <div >
+      <button
+        onClick={toggleMenu}
+        className="bg-[#0000FF] text-white font-semibold py-2 px-4 rounded-lg flex gap-x-2 items-center "
+      >
+        Logo
+        <FaChevronDown/>
+      </button>
+      
+      {showMenu && (
+        <div className="mt-4 p-4 w-48 border border-[#0000FF] rounded-lg bg-white shadow-md  ">
+          <div className="flex flex-col mb-4">
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              className="mb-2"
+            />
+            <div >
+              {handleImageUpload} 
+            </div>
+          </div>
+          <label className="block   mb-2">
+            Hauteur
+            <input
+              className="border p-2 rounded-md w-40 mb-4"
+              value={tempLogoTaille}
+              onChange={(e) => handleSizeChange(Number(e.target.value), setTempLogoTaille)}
+              placeholder="Entrez la hauteur"
+            />
+          </label>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UploadMenu;
---
+++ .history/vite.config_20250408114050.js
@@ -0,0 +1,11 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  server: {
+    host:'192.168.1.40',
+    port: 5173
+  }
+});
