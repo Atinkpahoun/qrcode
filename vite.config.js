d73c7aa36caf47df159970425ece5692f292e0f1
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2025-04-08 10:57:43 +0100

    New

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@composants": path.resolve(__dirname, "./src/composants"),
-    },
-  },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
 });
